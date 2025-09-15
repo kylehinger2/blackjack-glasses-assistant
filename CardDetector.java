1f14cfc4faf9581d7252dc2596457eb1e8ba1976
kylehinger2 <kylehinger2@gmail.com>
2025-09-14 21:50:53 -0400

    Create CardDetector.java
    placeholder

---
+++ CardDetector.java
@@ -0,0 +1,1 @@
+package com.example.blackjackassistant; public class CardDetector { public String detectCard(byte[] frameData) { // TODO: integrate YOLOv8 card recognition return "[Card Detector Placeholder]"; } }
